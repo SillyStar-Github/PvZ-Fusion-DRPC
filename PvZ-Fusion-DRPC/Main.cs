f8656015456cc52a0f3e04af3d63349d24a273ff
SillyStar-Github <90102598+SillyStar-Github@users.noreply.github.com>
2025-03-01 14:32:36 -0500

    init

---
+++ PvZ-Fusion-DRPC/Main.cs
@@ -0,0 +1,31 @@
+﻿using MelonLoader;
+using DiscordRichPresense;
+
+namespace PvZ_Fusion_DRPC
+{
+    public class Main : MelonMod
+    {
+        private const string DiscordAppID = "1343265059207381142";
+        public static readonly DiscordRpc.RichPresence Presence = new DiscordRpc.RichPresence();
+        public override void OnApplicationStart()
+        {
+            var handlers = new DiscordRpc.EventHandlers();
+            DiscordRpc.Initialize(DiscordAppID, ref handlers, false, string.Empty);
+            Presence.state = string.Empty;
+            Presence.details = string.Empty;
+            Presence.startTimestamp = default(long);
+            Presence.largeImageKey = "lanpiaopiao";
+            DiscordRpc.UpdatePresence(Presence);
+        }
+        public override void OnUpdate()
+        {
+            DiscordRpc.RunCallbacks();
+        }
+
+        public override void OnApplicationQuit()
+        {
+            DiscordRpc.Shutdown();
+        }
+
+    }
+}
---
+++ PvZ-Fusion-DRPC/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using MelonLoader;
+using PvZ_Fusion_DRPC;
+// ...
+[assembly: MelonInfo(typeof(PvZ_Fusion_DRPC.Main), "PvZ Fusion Discord Rich Presence", "1.0.0", "cassidy")]//MelonInfo(typeof(ClassOfMod), "Game Mod Name", "Version", "Author")
+[assembly: MelonGame("LanPiaoPiao", "PlantsVsZombiesRH")]
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PvZ-Fusion-DRPC")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PvZ-Fusion-DRPC")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5704fa18-961b-4a0e-9bf4-58c1cc1375f9")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
